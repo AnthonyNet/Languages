70e0246104fcca96c1c1672e1cd8874a5addbbcf
AnthonyZet <67970914+AnthonyZet@users.noreply.github.com>
2021-04-03 18:54:34 +0200

    before without materialize

--- partials/header.php
+++ partials/header.php
@@ -1,24 +1,54 @@
+<?php 
+	$page_name = basename($_SERVER['SCRIPT_NAME'], '.php');
+	if( $page_name == 'index') $page_name = 'home';
+?>
+
 <!DOCTYPE html>
 <html>
 
 <meta charset="utf-8">
 <title>Brainzone</title>
-
+<!--
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;1,300;1,400&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
 <link rel="stylesheet" href="../css/style.css">
 
 <header>
 
-
 <nav class="nav">
   <ul class="nav-list">
-  <li><a href="../assets/index.php">Hlavní stránka</a></li>
-    <li><a href="../assets/anglictina.php">Angličtina</a></li>
-	<li><a href="../assets/aj-irregular.php">AJ-Irregular verbs</a></li>
-    <li><a href="../assets/nemcina.php">Němčina</a></li>
+		<?php 
+			$pages = glob('*.php');
+			
+			$pages = array_reverse($pages);
+
+			foreach( $pages as $file) {
+				$page = basename($file, '.php');
+				if( $page == 'index') $page = 'home';
+
+				if($page_name == $page) echo '<li><strong>'.ucfirst($page) .'</strong></li>';
+				else echo '<li><a href="'. $file . '">'. ucfirst($page) .'</a></li>';
+				
+			}
+		?>
+ </ul>
+</nav>
+
+<!-- 
+	OLD NAV
+	<nav class="nav">
+  <ul class="nav-list">
+  <li><a href="index.php">Hlavní stránka</a></li>
+    <li><a href="anglictina.php">Angličtina</a></li>
+	<li><a href="aj-irregular.php">AJ-Irregular verbs</a></li>
+    <li><a href="nemcina.php">Němčina</a></li>
   </ul>
 </nav>
+-->
 
 	<!-- 
 			Back to top 
